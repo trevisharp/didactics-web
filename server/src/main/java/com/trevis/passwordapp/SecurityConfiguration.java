99a8e53d52ffcc4997df16097b57d18526b811ef
trevisharp <leonardotrevisansilio@gmail.com>
2024-11-05 16:13:30 -0300

    add jwt authentication on spring security

--- server/src/main/java/com/trevis/passwordapp/SecurityConfiguration.java
+++ server/src/main/java/com/trevis/passwordapp/SecurityConfiguration.java
@@ -1,27 +1,34 @@
 package com.trevis.passwordapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.trevis.passwordapp.dto.Token;
+import com.trevis.passwordapp.filters.JWTAuthenticationFilter;
+import com.trevis.passwordapp.services.JWTService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    
+    @Autowired
+    JWTService<Token> jwtService;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/**")
-                .permitAll()
-                .anyRequest()
-                .anonymous()
+                .requestMatchers("/user").permitAll()
+                .requestMatchers("/user/login").permitAll()
+                .anyRequest().authenticated()
             )
-            .rememberMe(Customizer.withDefaults())
+            .addFilterBefore(new JWTAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 }
--- server/src/main/java/com/trevis/passwordapp/controllers/PasswordController.java
+++ server/src/main/java/com/trevis/passwordapp/controllers/PasswordController.java
@@ -1,5 +1,6 @@
 package com.trevis.passwordapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/password")
 public class PasswordController {
     
-    
+    @GetMapping
+    public String generate()
+    {
+        return "senha complexa";
+    }
 
 }
